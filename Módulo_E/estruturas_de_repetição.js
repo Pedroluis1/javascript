40c5ca67ec1f0be76ed05c8b21b5e3f7afde3016
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2022-07-03 06:12:32 -0300

    commit

---
+++ Módulo_E/estruturas_de_repetição.js
@@ -0,0 +1,21 @@
+for(c = 1; c <=5; c++){
+    console.log(`passo${c}`)
+}
+
+/*--------------------------------------------*/
+
+//Estrutura de Repetição com teste lógico no inicio (while())
+/*var c = 1 
+while (c <= 5){
+    console.log(`passo ${c}`)
+    c++
+}*/
+
+/*---------------------------------------------*/
+
+//Estrutura de Repetição com teste lógico no final (do...while())
+/*var c = 1 
+do {
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 5)*/
