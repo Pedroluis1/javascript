159fa3b0597035edb6b020165f3348efdece5fe3
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2022-07-06 18:41:46 -0300

    commir

--- Módulo_E/ex17/modelo/tabuada.js
+++ Módulo_E/ex17/modelo/tabuada.js
@@ -1,12 +1,16 @@
 function gerar(){
     let num = document.getElementById('txtn')
-    let res = document.getElementById('res')
-    if (num.value.length <= 0) {
+    let res = document.getElementById('seltab')
+    if (num.value.length == 0) {
         window.alert('Por favor, digite um número.')
     } else {
         let n = Number(num.value)
+        res.innerHTML = ''
         for (var c = 1; c <= 10; c+=1) {
-            res.innerHTML += `${n} x ${c} = ${n*c}<br>`
+            let item = document.createElement('option')
+            item.text += `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            res.appendChild(item)
         }
     }
 }
