b7ac721bea66a179d6485bbbe5387a632c4b2c87
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2022-07-10 17:57:43 -0300

    Commit

---
+++ Módulo_F/função02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0){ 
+    // Se o n1 ou n2 não for definido e der NaN (not a number), em vez disso vai ser considerado como 0.
+    return n1 + n2
+}
+console.log(soma(5, 1)) 
