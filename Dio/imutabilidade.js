f634d8f68877e1be6f049f569429d57ea8da99a6
Pedro luís <93263907+Pedroluis1@users.noreply.github.com>
2022-08-11 23:28:27 -0300

    commit

--- Dio/imutabilidade.js
+++ Dio/imutabilidade.js
@@ -2,14 +2,36 @@ const user = {
     name = 'Guilherme',
     lastname = 'Cabrini da Silva'
 };
-
+/* O user não será alterado a referência dele, será feito uma cópia e alterará a cópia */
 function GetUserWithFullName(user){
     return{
-        ...user,
+        ...user, // spread operator (para cada propriedade do user, ele vai colocar dentro do objeto )
         fullName: `${user.name} ${user.lastname}`
-    }
+    } // objeto
 }
 
-const GetUserWithFullName = GetUserWithFullName(user);
+const GetUserWithFullName = GetUserWithFullName(user);// mesmo ojeto com os mesmo atributos do user, mais o fullname .
 
-console.log(UserWithFullName, user)
+console.log(UserWithFullName, user)// mesmo objeto com o fullname sem alterar a referência dele.
+
+
+
+console.log(GetUserWithFullName)// descobrir a diferença do console.log com Get e sem.
+
+/*EXEMPLO 2 -------------------------------------------------------------*/
+// Outra forma de imutabilidade
+const students = [
+    { name: 'Grace', grade: 7 },
+    { name: 'Jennifer', grade: 4},
+    { name: 'Paul', grade: 5}
+]
+
+function getApprovedStudents(studentsList){ // Função recebe a lista de alunos.
+    return studentsList.filter(student => student.grade >= 7) // retornando apenas os alunos aprovados.
+}
+
+console.log('Alunos Aprovados:')
+console.log(getApprovedStudents(students))
+
+console.log('Lista de alunos')
+console.log(students)
---
+++ Dio/var,let,const.js
@@ -0,0 +1,51 @@
+var test = 'example'
+
+(() => {// função de execução imediata
+    console.log(`Valor dentro da função "${test}"`)
+    
+    if (true) {
+        var test = 'example'
+        console.log(`Valor dentro do if "${test}"`)
+    }
+
+    if (true )
+
+    console.log(`Valor após a execução do if "${test}"`)
+})(); 
+
+// mesma coisa de:
+/*function test() {
+
+}();*/
+
+(() => {
+    let test = 'valor função'
+    console.log(`valor dentro da função "${test}"`)
+    if (true) {
+        let test = 'valor  if ';
+        console.log(`Valor dentro do if "${test}"`)
+    }
+
+    console.log(`valor após a execução do if "${test}"`)
+})
+
+
+
+
+
+
+
+
+/*
+
+// escopo global  Var
+
+{
+    // escopo bloco  Let ou const ( resoeitam o escopo de bloco )
+}
+
+function test() {
+    // escopo de função   Let ou const
+}
+
+*/
